9f45e4c4d1e03601ce18a90bcc26082398fb2a01
tanjim00 <tanjim.ac00@gmail.com>
2023-04-25 20:58:49 +0600

    product functionalities added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href="/">SonicZen Headphones</Link>
+        <Link href="/">SonicZen</Link>
       </p>
       <button type='button' className='cart-icon' onClick="">
         <AiOutlineShopping/>
